eb92bbcaf0147714f28ef8d7167dea014a4bbde1
Charles Carrier <charles.e.carrier@gmail.com>
2019-02-14 16:43:38 +0800

    getting comments to post to the backend

--- src/App.js
+++ src/App.js
@@ -7,17 +7,28 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      id: 0,
       toDos: []
     };
   }
 
   formChange = toDo => {
     this.setState({ toDos: [...this.state.toDos, toDo] });
+    let currentIds = this.state.data.map(data => data.id);
+    let idToBeAdded = 0;
+    while (currentIds.includes(idToBeAdded)) {
+      ++idToBeAdded;
+    }
+    // What the while statement does is look at current is loop through all the data in the state
+    // and check the data's id and if the id equals the current value of idToBeAdded, the value of
+    // idToBeAdded increases by 1. This ensures that we have unique id that is one larger than all existing ideas.
     axios.post("http://localhost:3001/api/putData", {
-      id: 10,
+      id: idToBeAdded,
       toDo: toDo
     });
   };
+
+  // so I need to figure how to push the id in with the toDo. That needs to be done with setState
 
   render() {
     return (
